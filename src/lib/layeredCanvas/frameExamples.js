de804335d8655f8d3f84b8a497cdca6da3522f4e
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2023-04-03 18:37:59 +0900

    refine frame examples

--- src/lib/layeredCanvas/frameExamples.js
+++ src/lib/layeredCanvas/frameExamples.js
@@ -1,123 +1,143 @@
 export const frameExamples = [
   {
-    margin: {
-      top: 4,
-      bottom: 4,
-      left: 8,
-      right: 8,
-    },
-    width: 180,
-    divider: {
-      spacing: 2,
-      slant: 0,
-    },
     bgColor: "white",
-    column: [
-      { height: 17 },
+    height: 100,
+    row: [
       {
-        height: 25,
+        margin: {
+          top: 4,
+          bottom: 4,
+          left: 8,
+          right: 8,
+        },
+        width: 180,
         divider: {
           spacing: 2,
           slant: 0,
         },
-        row: [{ width: 45 }, { width: 55 }],
-      },
-      { height: 17 },
-    ],
-  },
-  {
-    margin: {
-      top: 4,
-      bottom: 4,
-      left: 4,
-      right: 4,
-    },
-    width: 180,
-    bgColor: "white",
-    column: [
-      {
-        height: 180,
-      },
-    ],
-  },
-  {
-    margin: {
-      top: 1,
-      bottom: 1,
-      left: 2,
-      right: 2,
-    },
-    width: 3,
-    divider: {
-      spacing: 1,
-      slant: 0,
-    },
-    bgColor: "white",
-    column: [
-      {
-        height: 8,
-      },
-      {
-        height: 8,
-      },
-      {
-        height: 8,
-      },
-      {
-        height: 8,
-      },
-    ],
-  },
-  {
-    bgColor: "white",
-    column: [
-      {
-        height: 60,
-      },
-      {
-        bgColor: null,
-        row: [
+        column: [
+          { height: 17 },
           {
-            bgColor: null,
-            width: 30,
+            height: 25,
+            divider: {
+              spacing: 2,
+              slant: 0,
+            },
+            row: [{ width: 45 }, { width: 55 }],
+          },
+          { height: 17 },
+        ],
+      },
+    ],
+  },
+  {
+    bgColor: "white",
+    height: 100,
+    row: [
+      {
+        margin: {
+          top: 4,
+          bottom: 4,
+          left: 4,
+          right: 4,
+        },
+        width: 180,
+        column: [
+          {
+            height: 180,
+          },
+        ],
+      },
+    ]
+  },
+  {
+    bgColor: "white",
+    height: 100,
+    row: [
+      {
+        margin: {
+          top: 1,
+          bottom: 1,
+          left: 2,
+          right: 2,
+        },
+        width: 3,
+        divider: {
+          spacing: 1,
+          slant: 0,
+        },
+        column: [
+          {
+            height: 8,
+          },
+          {
+            height: 8,
+          },
+          {
+            height: 8,
+          },
+          {
+            height: 8,
+          },
+        ],
+      },
+    ]
+  },
+  {
+    bgColor: "white",
+    height: 100,
+    row: [
+      {
+        column: [
+          {
+            height: 60,
           },
           {
             bgColor: null,
-            column: [
+            row: [
               {
                 bgColor: null,
-                height: 30,
+                width: 30,
               },
               {
                 bgColor: null,
-                height: 30,
+                column: [
+                  {
+                    bgColor: null,
+                    height: 30,
+                  },
+                  {
+                    bgColor: null,
+                    height: 30,
+                  },
+                ],
+                divider: {
+                  spacing: 1,
+                  slant: 0,
+                },
+                width: 30,
               },
             ],
             divider: {
-              spacing: 1,
+              spacing: 2,
               slant: 0,
             },
-            width: 30,
+            height: 120,
           },
         ],
         divider: {
-          spacing: 2,
+          spacing: 4,
           slant: 0,
         },
-        height: 120,
+        margin: {
+          top: 4,
+          bottom: 4,
+          left: 4,
+          right: 4,
+        },
+        height: 180,
       },
-    ],
-    divider: {
-      spacing: 4,
-      slant: 0,
-    },
-    margin: {
-      top: 4,
-      bottom: 4,
-      left: 4,
-      right: 4,
-    },
-    height: 180,
+    ]
   },
   {
     bgColor: "transparent",
