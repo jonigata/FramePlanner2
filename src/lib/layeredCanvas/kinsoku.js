d620fb95f6f32149cca86546f77abee8b55658fc
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2023-06-01 07:32:49 +0900

    improve newLine processing of kinsoku

--- src/lib/layeredCanvas/kinsoku.js
+++ src/lib/layeredCanvas/kinsoku.js
@@ -18,8 +18,6 @@ const gyoumatuKinsokuTable = makeTable(`
 const maxBurasageDepth = 2;
 const maxMochikoshiDepth = 2;
 
-const newlineChar = '\n';
-
 function* kinsokuGenerator(overflowDetector, getNext) {
   let currentLine = '';  
   let buffered = '';
@@ -36,7 +34,7 @@ function* kinsokuGenerator(overflowDetector, getNext) {
 
   while (true) {
     const c = get();
-    if (c == null || c == newlineChar) {
+    if (c == null) {
       yield currentLine;
       if (c == null) { break; }
     } else {
@@ -61,7 +59,7 @@ function* kinsokuGenerator(overflowDetector, getNext) {
           let burasageDepth = maxBurasageDepth;
           while (0 < burasageDepth) {
             const c = get();
-            if (c == null || c == newlineChar) {
+            if (c == null) {
               break;
             }
             if (!gyoutouKinsokuTable.has(c)) {
@@ -80,21 +78,24 @@ function* kinsokuGenerator(overflowDetector, getNext) {
   }
 }
 
-export function kinsoku(overflowDetector, s) {
-  let i = 0;
-  const getNext = () => i < s.length ? s[i++] : null;
-  return Array.from(kinsokuGenerator(overflowDetector, getNext));
+export function kinsoku(overflowDetector, ss) {
+  let a = [];
+  for (let s of ss.split('\n')) {
+    let i = 0;
+    const getNext = () => i < s.length ? s[i++] : null;
+    a = a.concat(Array.from(kinsokuGenerator(overflowDetector, getNext)));
+  }
+  return a;
 }
-
 
 const exampleSentences = [
   "彼女は元気に挨拶した。「こんにちは！」｛長い間会っていなかった友人に｝",
   "インターネットを開いて最新のニュースを見た｛彼は驚いた表情で｝",
   "彼はパーティーに参加することを楽しみにしている。｛友人たちと一緒に｝",
-  "驚きと喜びが入り混じるニュースを聞いて、彼は目を丸くした。「本当に！？」",
+  "驚きと喜びが入り混じるニュースを聞いて、\n彼は目を丸くした。「本当に！？」",
   "彼が尋ねた。「これはどう使うのかな？」｛興味津々な顔で｝",
-  "雨が降ってきて、彼女はびしょ濡れになった。「なんてこった！」",
-  "サッカーチームは必勝を信じ、練習に励んでいる。「優勝するぞ！」と監督が叫んだ。",
+  "雨が降ってきて、彼女はびしょ\n濡れになった。「なんてこった！」",
+  "サッカーチームは\n必勝を信じ、練習に励んでいる。「優勝するぞ！」と監督が叫んだ。",
   "彼は敵を巧妙な動きで翻弄した。「こんなに速くて強いのか！」",
   "友人たちは言った。「おめでとう！成功を祝福するよ」",
   "彼の言葉には深い感動が込められていた。「君がここにいてくれて本当に嬉しい」",
@@ -102,6 +103,5 @@ const exampleSentences = [
 
 exampleSentences.forEach((s) => {
   console.log(s);
-  console.log(kinsoku((s) => 1 < s.length, s));
+  console.log(kinsoku((s) => 5 < s.length, s));
 });
-
