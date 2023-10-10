589b48eeb2d0c92693e5449b937f236285315e68
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2023-10-10 20:37:55 +0900

    fix hint timing

--- src/passiveToolTipStore.ts
+++ src/passiveToolTipStore.ts
@@ -4,14 +4,17 @@ import { writable } from "svelte/store";
 export const toolTipRequest = writable(null);
 
 export function toolTip(node, message) {
+  let timeoutId = null;
+
   const handleMouseEnter = (event) => {
-    setTimeout(() => {
+    timeoutId = setTimeout(() => {
       const r = node.getBoundingClientRect();
       const [cx, cy] = [r.left + r.width / 2, r.top + r.height / 2];
       toolTipRequest.set({ message, position: {x: cx, y: cy-20 }});
     }, 1000);
   }
   const handleMouseLeave = (event) => {
+    clearTimeout(timeoutId);
     toolTipRequest.set(null);
   }
   
