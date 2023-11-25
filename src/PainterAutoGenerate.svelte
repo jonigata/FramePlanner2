cb5a0a9b26acc8582f3f4d29fadce27b6727f51a
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2023-11-25 17:20:13 +0900

    tweak Manga Weaver

---
+++ src/PainterAutoGenerate.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { redrawToken } from './paperStore';
+  import { generateImageWithScribble } from "./sdwebui";
+
+  let busy: boolean;
+  let queued: boolean;
+
+  export async function doScribble(callGeneration, url, refered, prompt, lcm, target) {
+    if (!callGeneration) {
+      target.src = refered.src;
+      await target.decode();
+      $redrawToken = true;
+      return;
+    }
+
+    if (busy) { 
+      queued = true;
+      return; 
+    }
+
+    busy = true;
+    try {
+      const img = await generateImageWithScribble(url, refered, prompt, lcm);
+      target.src = img.src;
+      await target.decode();
+    } catch (e) {
+      console.log(e);
+      // toastStore.trigger({ message: `画像生成エラー: ${e}`, timeout: 3000});
+    }
+    busy = false;
+    $redrawToken = true;
+
+    if (queued) {
+      queued = false;
+      doScribble(callGeneration, url, refered, prompt, lcm, target);
+    }
+  }
+</script>
