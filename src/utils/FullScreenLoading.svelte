64395ec939a8ee9386a417857b751e555e40168c
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2024-01-30 07:49:12 +0900

    commit files

---
+++ src/utils/FullScreenLoading.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { openModal, closeModal } from 'svelte-modals'
+  import Loading from './Loading.svelte'
+  import { loading } from './loadingStore'
+
+  $: onLoadingChange($loading);
+  function onLoadingChange(loading) {
+    if (loading) {
+      openModal(Loading);
+    } else {
+      closeModal();
+    }
+  }
+</script>
---
+++ src/utils/Loading.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { loading } from './loadingStore';
+  import { ProgressRadial } from '@skeletonlabs/skeleton';
+</script>
+
+{#if $loading}
+  <div role="dialog" class="modal">
+    <div class="contents">
+      <ProgressRadial/>
+    </div>
+  </div>
+{/if}
+
+<style>
+  .modal {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+    position: relative;
+  }
+</style>
