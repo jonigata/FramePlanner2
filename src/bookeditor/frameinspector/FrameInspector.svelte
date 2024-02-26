9207159c396fdf3b5bb8912e7b5f52bdc4c38a82
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2024-02-26 18:27:19 +0900

    implement film ui

--- src/bookeditor/frameinspector/FrameInspector.svelte
+++ src/bookeditor/frameinspector/FrameInspector.svelte
@@ -2,6 +2,9 @@
   import writableDerived from "svelte-writable-derived";
   import { draggable } from '@neodrag/svelte';
   import { type FrameInspectorPosition, frameInspectorTarget, frameInspectorPosition } from './frameInspectorStore';
+  import FrameInspectorFilm from "./FrameInspectorFilm.svelte";
+  import { Film } from "../../lib/layeredCanvas/dataModels/frameTree";
+  import { redrawToken } from "../bookStore"
 
   let adjustedPosition = { x: window.innerWidth - 350 - 16, y: 16 };
   let innerWidth = window.innerWidth;
@@ -29,6 +32,20 @@
     adjustedPosition = {x: offsetX, y: offsetY};
   }
 
+  function onNewFilm(e: CustomEvent<string>) {
+    const image = new Image();
+    image.src = e.detail;
+
+    const film = new Film();
+    film.image = image;
+    film.n_scale = 1;
+    film.n_translation = [0, 0];
+    film.rotation = 0;
+    film.reverse = [1, 1];
+    $frame.filmStack.films.push(film);
+    $redrawToken = true;
+  }
+
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight/>
@@ -38,6 +55,12 @@
   <div class="frame-inspector variant-glass-surface rounded-container-token vbox gap" use:draggable={{ position: adjustedPosition, onDrag: onDrag ,handle: '.title-bar'}} bind:this={inspector}>    
     <div class="title-bar variant-filled-surface rounded-container-token">
       コマ
+    </div>
+    <div class="film-stack vbox gap">
+      <FrameInspectorFilm film={null} on:new-film={onNewFilm}/>
+      {#each $frame.filmStack.films.toReversed() as film}
+        <FrameInspectorFilm film={film}/>
+      {/each}
     </div>
   </div>
 </div>
@@ -67,5 +90,10 @@
     flex-direction: column;
     align-items: center;
     position: relative;
+  }
+  .film-stack {
+    overflow-y: auto;
+    width: 100%;
+    height: 400px;
   }
 </style>
---
+++ src/bookeditor/frameinspector/FrameInspectorFilm.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import { createEventDispatcher, onMount } from 'svelte';
+  import type { Film } from "../../lib/layeredCanvas/dataModels/frameTree";
+
+  export let film: Film;
+
+  const dispatch = createEventDispatcher();
+
+  function onDragOver(ev: DragEvent) {
+    ev.preventDefault();
+    ev.stopPropagation();
+  }
+
+  async function onDrop(ev: DragEvent) {
+    ev.preventDefault();
+    ev.stopPropagation();
+    const dt = ev.dataTransfer;
+    const files = dt.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      console.log(file.type)
+      if (file.type.startsWith("image/")) {
+        const imageURL = URL.createObjectURL(file);
+        dispatch('new-film', imageURL);
+      }
+    }
+  }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+  class="film variant-soft-tertiary"
+  on:dragover={onDragOver}
+  on:drop={onDrop}>
+  {#if !film}
+    <div class="new-film">
+      ＋
+    </div>
+  {:else}
+    <img class="film-content" src={film.image.src} alt="film"/>
+  {/if}
+</div>
+
+<style>
+  .film {
+    width: 100%;
+    height: 100px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 8px;
+  }
+  .new-film {
+    font-size: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100px;
+  }
+  .film-content {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  
+</style>
