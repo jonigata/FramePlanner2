2b75ae891073f9ba1087091ffe6bb1f5e9390e13
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2024-07-08 22:02:28 +0900

    refine drawer(WIP)

--- src/utils/Drawer.svelte
+++ src/utils/Drawer.svelte
@@ -1,105 +1,105 @@
 <script>
-  import { createEventDispatcher } from 'svelte'
+  import { createEventDispatcher } from "svelte";
 
-  export let open         = false;
-  export let duration     = 0.2;
-  export let placement    = "left";
-  export let size         = null;
+  export let open = false;
+  export let duration = 0.2;
+  export let placement = "left";
+  export let size = null;
+  export let overlay = true;
 
-  const dispatch = createEventDispatcher()
+  const dispatch = createEventDispatcher();
 
   $: style = `--duration: ${duration}s; --size: ${size};`;
 
-  function handleClickAway () {
-      dispatch('clickAway')
+  function handleClickAway() {
+    dispatch("clickAway");
   }
-
 </script>
 
 <aside class="drawer" class:open {style}>
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <!-- svelte-ignore a11y-no-static-element-interactions -->
-  <div class="overlay" on:click={handleClickAway} />
+  {#if overlay}
+    <div class="overlay" on:click={handleClickAway} />
+  {/if}
   <div class="panel {placement}" class:size>
     <slot />
   </div>
 </aside>
 
 <style>
-
   .drawer {
-      position: fixed;
-      top: 0;
-      left: 0;
-      height: 100%;
-      width: 100%;
-      z-index: -1;
-      transition: z-index var(--duration) step-end;
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    z-index: -1;
+    transition: z-index var(--duration) step-end;
   }
 
   .drawer.open {
-      z-index: 99;
-      transition: z-index var(--duration) step-start;
+    z-index: 99;
+    transition: z-index var(--duration) step-start;
   }
 
   .overlay {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background: rgba(100, 100, 100, 0.5);
-      opacity: 0;
-      z-index: 2;
-      transition: opacity var(--duration) ease;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(100, 100, 100, 0.5);
+    opacity: 0;
+    z-index: 2;
+    transition: opacity var(--duration) ease;
   }
 
   .drawer.open .overlay {
-      opacity: 1;
+    opacity: 1;
   }
 
   .panel {
-      position: fixed;
-      width: 100%;
-      height: 100%;
-      background: white;
-      z-index: 3;
-      transition: transform var(--duration) ease;
-      overflow: auto;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background: white;
+    z-index: 3;
+    transition: transform var(--duration) ease;
+    overflow: auto;
   }
 
   .panel.left {
-      left: 0;
-      transform: translate(-100%, 0);
+    left: 0;
+    transform: translate(-100%, 0);
   }
 
   .panel.right {
-      right: 0;
-      transform: translate(100%, 0);
+    right: 0;
+    transform: translate(100%, 0);
   }
 
   .panel.top {
-      top: 0;
-      transform: translate(0, -100%);
+    top: 0;
+    transform: translate(0, -100%);
   }
 
   .panel.bottom {
-      bottom: 0;
-      transform: translate(0, 100%);
+    bottom: 0;
+    transform: translate(0, 100%);
   }
 
   .panel.left.size,
   .panel.right.size {
-      max-width: var(--size);
+    max-width: var(--size);
   }
 
   .panel.top.size,
   .panel.bottom.size {
-      max-height: var(--size);
+    max-height: var(--size);
   }
 
   .drawer.open .panel {
-      transform: translate(0, 0);
+    transform: translate(0, 0);
   }
-
 </style>
