304bbe4e20fa3ec63df7a97f2c3fc5b6f7e265c1
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2024-08-13 23:21:09 +0900

    remove console.log

--- src/lib/layeredCanvas/dataModels/film.ts
+++ src/lib/layeredCanvas/dataModels/film.ts
@@ -67,7 +67,6 @@ export class Film  {
     const image = this.media;
     const imageSize = Math.min(image.naturalWidth, image.naturalHeight) ;
     const pageSize = Math.min(paperSize[0], paperSize[1]);
-    console.log("setShiftedScale", paperSize, scale, image.naturalWidth, image.naturalHeight);
     this.n_scale = scale / (pageSize / imageSize);
   }
 
