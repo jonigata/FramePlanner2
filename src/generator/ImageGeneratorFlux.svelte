bd6ebbcf338ce6eaa1da28cd5230af615bc570df
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2024-09-29 23:22:34 +0900

    add copyToClipboard to image generatior prompt

--- src/generator/ImageGeneratorFlux.svelte
+++ src/generator/ImageGeneratorFlux.svelte
@@ -13,8 +13,11 @@
   import { ProgressRadial } from '@skeletonlabs/skeleton';
   import { createCanvasFromImage } from '../utils/imageUtil';
   import { type ImagingContext, type Mode, generateFluxImage } from '../utils/feathralImaging';
+  import { toolTip } from '../utils/passiveToolTipStore';
   import SliderEdit from '../utils/SliderEdit.svelte';
   import FluxModes from './FluxModes.svelte';
+
+  import clipboardIcon from '../assets/clipboard.png';
 
   export let busy: boolean;
   export let prompt: string;
@@ -96,6 +99,11 @@
     busy = false;
   }
 
+  function copyToClipboard() {
+    navigator.clipboard.writeText(prompt);
+    toastStore.trigger({ message: 'クリップボードにコピーしました', timeout: 1500});
+  }
+
   onMount(async () => {
     $onlineAccount.feathral = await getFeathral();
     console.log($onlineAccount.feathral);
@@ -107,21 +115,28 @@
   {#if $onlineStatus === 'signed-in'}
   <p><Feathral/></p>
 
-  <p>モード</p>
+  <h2>モード</h2>
   <div class="vbox left gap-2 mode">
     <FluxModes bind:mode={mode}/>
   </div>
 
-  <p>スタイル</p>
+  <h2>スタイル</h2>
   <textarea class="w-96 textarea" bind:value={postfix} use:persistentText={{store:'imaging', key:'style', onLoad: (v) => postfix = v}}/>
-  <p>プロンプト</p>
-  <textarea class="textarea" bind:value={prompt}/>
+  <h2>プロンプト</h2>
+  <div class="textarea-container">
+    <textarea class="textarea h-32" bind:value={prompt}/>
+    <div class="icon-container flex flex-row-reverse gap-2">
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+      <img src={clipboardIcon} alt="クリップボードにコピー" on:click={copyToClipboard} use:toolTip={"クリップボードにコピー"} />
+    </div>
+  </div>        
 <!--
   <p>negative prompt</p>
   <textarea bind:value={imageRequest.negative_prompt}/>
 -->
 
-  <div class="hbox gap-5">
+  <div class="hbox gap-5 mt-4">
 <!--
     <div class="vbox" style="width: 400px;">
       <SliderEdit label="width" bind:value={imageRequest.width} min={512} max={1024} step={256}/>
@@ -147,7 +162,7 @@
     </div>
   </div>
 
-  <div class="flex flex-row gap-5 w-full items-center">
+  <div class="flex flex-row gap-5 w-full items-center mt-4">
     <button disabled={busy || $onlineAccount.feathral < 1} class="bg-primary-500 text-white hover:bg-primary-700 focus:bg-primary-700 active:bg-primary-900 generate-button" on:click={generate}>
       <div class="flex justify-center items-center h-6">
         {#if busy}
@@ -174,7 +189,6 @@
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    gap: 10px;
     margin: 16px;
   }
   textarea {
@@ -185,5 +199,25 @@
   }
   .warning {
     color: #d22;
+  }
+  h2 {
+    font-family: '源暎エムゴ';
+    font-size: 18px;
+    display: flex;
+    align-items: center;
+    margin-top: 8px;
+  }
+  .textarea-container {
+    position: relative;
+    width: 100%;
+  }
+  .icon-container {
+    position: absolute;
+    right: 16px;
+    top: -30px;
+  }
+  .icon-container img {
+    width: 24px;
+    height: 24px;
   }
 </style>
