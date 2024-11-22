7767442030280a048a0ef8ea6c14d83a3b7612e2
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2024-11-22 21:54:43 +0900

    show publication info

--- src/ViewerApp.svelte
+++ src/ViewerApp.svelte
@@ -2,28 +2,39 @@
   import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
   import '@skeletonlabs/skeleton/styles/all.css';
 
+  import { onMount } from "svelte";
+  import { getPublication, type PublicationContent } from './firebase';
+
   import MangaView from "./mangaview/MangaView.svelte";
   import { Modals } from 'svelte-modals'
   import FullScreenLoading from './utils/FullScreenLoading.svelte';
+  import PublicationInfo from './mangaview/PublicationInfo.svelte';
+
+  let publication: PublicationContent;
+
+  onMount(async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log("URLParams", urlParams);
+    let envelope = urlParams.get('envelope');
+    if (envelope === null) {
+      const envelope2 = window.location.pathname.split('/').pop();
+      if (!envelope2) {
+        console.error("envelope not found");
+        return;
+      }
+      envelope = envelope2;
+    }
+
+    publication = await getPublication(envelope);
+  });
 </script>
 
 <!-- 左右 -->
 <div class="flex w-full h-full">
 <aside class="w-1/4 h-full">
-  <h2>タイトル</h2>
-  <p>あした晴れる</p>
-  <h2>作者</h2>
-  <p>佐藤十四郎</p>
-  <h2>説明</h2>
-  <p>
-    都会の片隅にある古い神社には、人々の心の“影”が集まる異世界が存在していた。そこを守る白い狐の霊「白狐」は、人々の悩みや恐れを影の世界に引き込み、浄化していた。
-
-    ある日、自分の兄が突然失踪した少女・希美（のぞみ）が、影の世界に迷い込み、影を渡る力を手にする。希美と白狐は共に影の世界を巡り、心の影に潜む化身と対峙しながら兄の行方を追う。しかし、影の世界にはさらに強大な“真の影”が潜んでおり、二人はその存在に立ち向かうことを決意する。
-    
-    希美は兄を見つけ出し、白狐は神社を守ることができるのか──？  
-  </p>
-  <h2>作成日</h2>
-  <p>2021/10/10</p>
+  {#if publication}
+    <PublicationInfo publication={publication}/>
+  {/if}
 </aside>
 <main class="w-full h-full">
   <MangaView/>
@@ -48,16 +59,5 @@
   aside {
     padding: 16px;
     border-right: 1px solid #ddd;
-  }
-  aside p {
-    font-family: '源暎アンチック';
-    margin-left: 16px;
-    margin-bottom: 16px;
-  }
-  h2 {
-    font-family: '源暎エムゴ';
-    font-size: 1.2rem;
-    color: #666;
-    margin-bottom: -8px;
   }
 </style>
---
+++ src/mangaview/PublicationInfo.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import type { PublicationContent } from '../firebase';
+
+  export let publication: PublicationContent;
+
+  let formattedDate: string;
+
+  $: formattedDate = formatDate(publication.created_at);
+
+  function formatDate(isoString: string): string {
+    const date = new Date(isoString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}年${month}月${day}日`;
+  }
+  
+</script>
+
+<h2>タイトル</h2>
+<p>{publication.title}</p>
+<h2>作者</h2>
+<p>{publication.author_display_name}</p>
+<h2>説明</h2>
+<p>
+  {publication.description}
+</p>
+<h2>作成日</h2>
+<p>{formattedDate}</p>
+
+<style>
+p {
+  font-family: '源暎アンチック';
+  margin-left: 16px;
+  margin-bottom: 16px;
+}
+h2 {
+  font-family: '源暎エムゴ';
+  font-size: 1.2rem;
+  color: #666;
+  margin-bottom: -8px;
+}
+</style>
