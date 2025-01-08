fa174139ec16584f06a02a57bf1522b8e4e27a9d
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2025-01-08 14:35:15 +0900

    refactor

--- src/filemanager/developmentFileSystem.ts
+++ src/filemanager/developmentFileSystem.ts
@@ -1,10 +1,10 @@
 import { type FileSystem, makeFolders } from '../lib/filesystem/fileSystem';
 import { MockFileSystem } from '../lib/filesystem/mockFileSystem';
+import { specialFolders } from './specialFolders';
 
 export async function buildFileSystem(): Promise<FileSystem> {
   const fs = new MockFileSystem();
 
-  const specialFolders = ['デスクトップ', 'キャビネット', 'ごみ箱', 'テンプレート', '画像', '動画', 'プリファレンス', '素材'];
   await makeFolders(fs, specialFolders);
 
   return fs;
