aae9fefed1ab48c6d4b8620729b1dfcb7c921833
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2025-04-18 17:34:49 +0900

    refactor bubble rendering logic by moving related functions to renderBubble.ts

--- src/lib/layeredCanvas/layers/paperRendererLayer.ts
+++ src/lib/layeredCanvas/layers/paperRendererLayer.ts
@@ -1,13 +1,13 @@
+import { reverse2D } from "../tools/geometry/geometry";
 import { LayerBase } from "../system/layeredCanvas";
-import { type Vector, reverse2D, multiply2D, ceil2D, scale2D, reciprocal2D } from "../tools/geometry/geometry";
-import { drawBubble, getPath, drawPath, type DrawMethod } from "../tools/draw/bubbleGraphic";
+import { getPath } from "../tools/draw/bubbleGraphic";
 import { trapezoidBoundingRect, trapezoidPath } from "../tools/geometry/trapezoid";
 import { findLayoutAt, calculatePhysicalLayout, FrameElement } from "../dataModels/frameTree";
 import { drawFilmStack } from "../tools/draw/drawFilmStack";
 import type { Layout } from "../dataModels/frameTree";
-import { drawText, measureText, type Direction } from "../tools/draw/drawText";
 import type { Bubble, BubbleRenderInfo } from "../dataModels/bubble";
 import { makePlainCanvas } from "../tools/imageUtil";
+import { renderBubbles, renderBubbleBackground, renderBubbleForeground } from "../tools/draw/renderBubble";
 
 type InheritanceContext = {
   borderColor: string, 
@@ -60,7 +60,7 @@ export class PaperRendererLayer extends LayerBase {
     }
 
     if (depth === 1) {
-      this.renderBubbles(ctx, floatingBubbles);
+      renderBubbles(ctx, this.getPaperSize(), floatingBubbles);
     }
   }
 
@@ -234,7 +234,7 @@ export class PaperRendererLayer extends LayerBase {
 
       if (embeddedBubbles.has(layout)) {
         const bubbles = embeddedBubbles.get(layout)!;
-        this.renderBubbles(ctx, bubbles);
+        renderBubbles(ctx, this.getPaperSize(), bubbles);
       }
   
       // unclip
@@ -255,88 +255,7 @@ export class PaperRendererLayer extends LayerBase {
     }
   }
 
-  renderBubbles(ctx: CanvasRenderingContext2D, bubbles: Bubble[]) {
-    for (let bubble of bubbles) {
-      this.renderBubbleBackground(ctx, bubble);
-      this.renderBubbleForeground(ctx, bubble, false);
-    }
-    for (let bubble of bubbles) {
-      this.renderBubbleForeground(ctx, bubble, true);
-    }
-  }
-
-  renderBubbleBackground(ctx: CanvasRenderingContext2D, bubble: Bubble) {
-    if (bubble.parent) { return; }
-
-    const paperSize = this.getPaperSize();
-    const size = bubble.getPhysicalSize(paperSize);
-    const strokeWidth = bubble.getPhysicalStrokeWidth(paperSize);
-    const [x0, y0, w, h] = bubble.getPhysicalRect(paperSize);
-
-    ctx.save();
-    ctx.translate(...bubble.getPhysicalCenter(paperSize));
-    ctx.rotate((-bubble.rotation * Math.PI) / 180);
-
-    // fill/stroke設定
-    ctx.fillStyle = bubble.hasEnoughSize(paperSize) ? bubble.fillColor : "rgba(255, 128, 0, 0.9)";;
-    ctx.strokeStyle = 0 < strokeWidth ? bubble.strokeColor : "rgba(0, 0, 0, 0)";
-    ctx.lineWidth = strokeWidth;
-    
-    // shape背景描画
-    this.drawBubble(ctx, size, 'fill', bubble);
-    this.drawBubble(ctx, size, 'clip', bubble);
-
-    // 画像描画
-    drawFilmStack(ctx, bubble.filmStack, paperSize);
-
-    ctx.restore();
-  }
-
-  renderBubbleForeground(ctx: CanvasRenderingContext2D, bubble: Bubble, drawsUnited: boolean) {
-    if (bubble.parent) {
-      if (!drawsUnited) { return; }
-    } else {
-      if (drawsUnited) { return; }
-    }
-
-    const paperSize = this.getPaperSize();
-    const size = bubble.getPhysicalSize(paperSize);
-    const center = bubble.getPhysicalCenter(paperSize);
-    const strokeWidth = bubble.getPhysicalStrokeWidth(paperSize);
-
-    ctx.save();
-    ctx.translate(...center);
-    ctx.rotate((-bubble.rotation * Math.PI) / 180);
-
-    ctx.save();
-    this.drawBubble(ctx, size, 'clip', bubble);
-
-    // テキスト描画
-    if (bubble.text && !bubble.hidesText) {
-      this.drawText(ctx, bubble);
-    }
-    ctx.restore();
-
-    // shape枠描画
-    ctx.strokeStyle = 0 < strokeWidth ? bubble.strokeColor : "rgba(0, 0, 0, 0)";
-    ctx.lineWidth = strokeWidth;
-    this.drawBubble(ctx, size, 'stroke', bubble);
-
-    ctx.restore();
-  }
-
-  drawBubble(ctx: CanvasRenderingContext2D, size: Vector, method: DrawMethod, bubble: Bubble) {
-    const ri = bubble.renderInfo!;
-    if (ri.unitedPath) {
-      drawPath(ctx, method, ri.unitedPath, bubble.optionContext);
-    } else if (!bubble.parent) {
-      if (ri.path) {
-        drawPath(ctx, method, ri.path, bubble.optionContext);
-      } else {
-        drawBubble(ctx, method, bubble.text, size, bubble.shape, bubble.optionContext);
-      }
-    }
-  }
+  // バブル描画関連のコードはsrc/lib/layeredCanvas/tools/draw/renderBubble.tsに移動
 
   drawFilms(ctx: CanvasRenderingContext2D, layout: Layout) {
     const paperSize = this.getPaperSize();
@@ -349,122 +268,7 @@ export class PaperRendererLayer extends LayerBase {
     ctx.restore();
   }
 
-  drawText(targetCtx: CanvasRenderingContext2D, bubble: Bubble) {
-    const transform = targetCtx.getTransform();
-    const dpr = this.supportsDpr ? (window.devicePixelRatio ?? 1) : 1;
-    const viewScale: Vector = [transform.a * dpr, transform.d * dpr];
-
-    const paperSize = this.getPaperSize();
-    const size = bubble.getPhysicalSize(paperSize);
-    const fontSize = bubble.getPhysicalFontSize(paperSize);
-    const offset = bubble.getPhysicalOffset(paperSize);
-    const outlineWidth = bubble.getPhysicalOutlineWidth(paperSize);
-
-    const [w, h] = ceil2D(multiply2D(size, viewScale));
-    if (w < 1 || h < 1) { return; }
-
-    const ri = bubble.renderInfo!;
-    const ss = `${bubble.fontStyle} ${bubble.fontWeight} ${fontSize}px '${bubble.fontFamily}'`;
-
-    // let startTime = performance.now();
-
-    const c = {
-      viewScale: ceil2D(scale2D(viewScale, 1000)),
-      size: size,
-      offset: offset,
-      fontStyle: bubble.fontStyle,
-      fontWeight: bubble.fontWeight,
-      fontSize: fontSize,
-      lineSkip: bubble.lineSkip,
-      charSkip: bubble.charSkip,
-      fontFamily: bubble.fontFamily,
-      text: bubble.text,
-      direction: bubble.direction,
-      autoNewline: bubble.autoNewline,
-      fontColor: bubble.fontColor,
-      outlineWidh: outlineWidth,
-      outlineColor: bubble.outlineColor,
-      fontRenderVersion: bubble.fontRenderVersion,
-      fontCheck: document.fonts.check(ss),
-    };
-    const json = JSON.stringify(c);
-    // console.log(`stringify took ${performance.now() - startTime} ms, ${json.length} bytes`);
-    if (ri.textJson != json) {
-      // startTime = performance.now();
-
-      // 変更が起きたときのみ
-      ri.textJson = json;
-
-      ri.textCanvas = document.createElement('canvas');
-      ri.textCtx = ri.textCanvas.getContext('2d')!;
-  
-      const canvas = ri.textCanvas;
-      const ctx = ri.textCtx;
-  
-      canvas.width = w;
-      canvas.height = h;
-
-      // 青で塗りつぶす
-      // ctx.fillStyle = 'blue';
-      // ctx.fillRect(0, 0, w, h);
-  
-      ctx.translate(w * 0.5, h * 0.5);
-  
-      ctx.scale(...viewScale);
-      ctx.translate(...offset);
-
-      ctx.font = ss; // horizontal measureより先にないとだめ
-      //const text = `${bubble.text}:${bubble.pageNumber}`;
-      let text = bubble.text;
-      if (!document.fonts.check(`${bubble.fontStyle} ${bubble.fontWeight} 20px '${bubble.fontFamily}'`)) {
-        // どうもcheckが信用ならないので以下のコードは潰す
-        // text = "ロード中……";
-      }
-
-      const baselineSkip = fontSize * 1.5 * (1.0 + bubble.lineSkip);
-      const charSkip = fontSize * (1.0 + bubble.charSkip);
-      const rubySize = bubble.rubySize;
-      const rubyDistance = bubble.rubyDistance;
-      const m = measureText(bubble.direction as Direction, ctx, size[0] * 0.85, size[1] * 0.85, text, baselineSkip, charSkip, bubble.autoNewline);
-      const [tw, th] = [m.width, m.height];
-      const r = { x: - tw * 0.5, y: - th * 0.5, width: tw, height: th };
-  
-      // 本体
-      ctx.globalCompositeOperation = 'source-over';
-      ctx.fillStyle = bubble.fontColor;
-      drawText(bubble.direction as Direction, ctx, 'fill', r, text, baselineSkip, charSkip, rubySize, rubyDistance, m, bubble.autoNewline);
-  
-      // フチ
-      if (0 < outlineWidth) {
-        ctx.globalCompositeOperation = 'destination-over';
-        ctx.strokeStyle = bubble.outlineColor;
-        ctx.lineWidth = outlineWidth;
-        ctx.font = ss;
-        ctx.lineJoin = 'round';
-        drawText(bubble.direction as Direction, ctx, 'stroke', r, text, baselineSkip, charSkip, rubySize, rubyDistance, m, bubble.autoNewline);
-      }
-
-      // console.log(`rendering took ${performance.now() - startTime} ms`);
-    }
-
-    // 描き戻し
-    const canvas = ri.textCanvas;
-    try {
-      const rotation = Math.round(bubble.rotation * 10) / 10;
-
-      targetCtx.save();
-      if (rotation === 0 || rotation === 90 || rotation === 180 || rotation === 270) {
-        targetCtx.imageSmoothingEnabled = false;
-      }
-      targetCtx.scale(...reciprocal2D(viewScale));
-      targetCtx.drawImage(canvas, 0 - w * 0.5, 0 - h * 0.5);
-      targetCtx.restore();
-    }
-    catch (e) {
-      console.log(w, h, canvas.width, canvas.height, size);
-      throw e;
-    }
-  }
+  // バブル描画関連のコードはsrc/lib/layeredCanvas/tools/draw/renderBubble.tsに移動
 
   setBubbles(bubbles: Bubble[]) {
     this.rawBubbles = bubbles;
@@ -512,14 +316,15 @@ export class PaperRendererLayer extends LayerBase {
       const ri = bubble.renderInfo;
       if (bubble.parent != null) { continue; }
       const { canvas, ctx } = makeCanvas();
-      this.renderBubbleBackground(ctx, bubble);
-      this.renderBubbleForeground(ctx, bubble, false);
+      const paperSize = this.getPaperSize();
+      renderBubbleBackground(ctx, paperSize, bubble);
+      renderBubbleForeground(ctx, paperSize, bubble, false);
       bubbleCanvases[bubble.uuid] = canvas;
     }
     for (let bubble of bubbles) {
       if (bubble.parent == null) { continue; }
       const ctx = bubbleCanvases[bubble.parent].getContext('2d')!;
-      this.renderBubbleForeground(ctx, bubble, true);
+      renderBubbleForeground(ctx, this.getPaperSize(), bubble, true);
     }
 
     return { frames: canvases, bubbles: Object.values(bubbleCanvases) };
---
+++ src/lib/layeredCanvas/tools/draw/renderBubble.ts
@@ -0,0 +1,196 @@
+import { type Vector, multiply2D, ceil2D, scale2D, reciprocal2D } from "../geometry/geometry";
+import { drawBubble as drawBubbleShape, drawPath, type DrawMethod } from "./bubbleGraphic";
+import { drawFilmStack } from "./drawFilmStack";
+import { drawText, measureText, type Direction } from "./drawText";
+import type { Bubble } from "../../dataModels/bubble";
+
+
+/**
+ * バブルの描画処理をまとめたユーティリティ
+ */
+
+export function renderBubbles(ctx: CanvasRenderingContext2D, paperSize: Vector, bubbles: Bubble[]) {
+  for (let bubble of bubbles) {
+    renderBubbleBackground(ctx, paperSize, bubble);
+    renderBubbleForeground(ctx, paperSize, bubble, false);
+  }
+  for (let bubble of bubbles) {
+    renderBubbleForeground(ctx, paperSize, bubble, true);
+  }
+}
+
+export function renderBubbleBackground(ctx: CanvasRenderingContext2D, paperSize: Vector, bubble: Bubble) {
+  if (bubble.parent) { return; }
+
+  const size = bubble.getPhysicalSize(paperSize);
+  const strokeWidth = bubble.getPhysicalStrokeWidth(paperSize);
+
+  ctx.save();
+  ctx.translate(...bubble.getPhysicalCenter(paperSize));
+  ctx.rotate((-bubble.rotation * Math.PI) / 180);
+
+  // fill/stroke設定
+  ctx.fillStyle = bubble.hasEnoughSize(paperSize) ? bubble.fillColor : "rgba(255, 128, 0, 0.9)";;
+  ctx.strokeStyle = 0 < strokeWidth ? bubble.strokeColor : "rgba(0, 0, 0, 0)";
+  ctx.lineWidth = strokeWidth;
+  
+  // shape背景描画
+  drawBubbleElement(ctx, paperSize, bubble, size, 'fill');
+  drawBubbleElement(ctx, paperSize, bubble, size, 'clip');
+
+  // 画像描画
+  drawFilmStack(ctx, bubble.filmStack, paperSize);
+
+  ctx.restore();
+}
+
+export function renderBubbleForeground(ctx: CanvasRenderingContext2D, paperSize: Vector, bubble: Bubble, drawsUnited: boolean) {
+  if (bubble.parent) {
+    if (!drawsUnited) { return; }
+  } else {
+    if (drawsUnited) { return; }
+  }
+
+  const size = bubble.getPhysicalSize(paperSize);
+  const center = bubble.getPhysicalCenter(paperSize);
+  const strokeWidth = bubble.getPhysicalStrokeWidth(paperSize);
+
+  ctx.save();
+  ctx.translate(...center);
+  ctx.rotate((-bubble.rotation * Math.PI) / 180);
+
+  ctx.save();
+  drawBubbleElement(ctx, paperSize, bubble, size, 'clip');
+
+  // テキスト描画
+  if (bubble.text && !bubble.hidesText) {
+    drawBubbleText(ctx, paperSize, bubble, true);
+  }
+  ctx.restore();
+
+  // shape枠描画
+  ctx.strokeStyle = 0 < strokeWidth ? bubble.strokeColor : "rgba(0, 0, 0, 0)";
+  ctx.lineWidth = strokeWidth;
+  drawBubbleElement(ctx, paperSize, bubble, size, 'stroke');
+
+  ctx.restore();
+}
+
+function drawBubbleElement(ctx: CanvasRenderingContext2D, paperSize: Vector, bubble: Bubble, size: Vector, method: DrawMethod) {
+  const ri = bubble.renderInfo!;
+  if (ri.unitedPath) {
+    drawPath(ctx, method, ri.unitedPath, bubble.optionContext);
+  } else if (!bubble.parent) {
+    if (ri.path) {
+      drawPath(ctx, method, ri.path, bubble.optionContext);
+    } else {
+      drawBubbleShape(ctx, method, bubble.text, size, bubble.shape, bubble.optionContext);
+    }
+  }
+}
+
+function drawBubbleText(targetCtx: CanvasRenderingContext2D, paperSize: Vector, bubble: Bubble, supportsDpr: boolean = true) {
+  const transform = targetCtx.getTransform();
+  const dpr = supportsDpr ? (window.devicePixelRatio ?? 1) : 1;
+  const viewScale: Vector = [transform.a * dpr, transform.d * dpr];
+
+  const size = bubble.getPhysicalSize(paperSize);
+  const fontSize = bubble.getPhysicalFontSize(paperSize);
+  const offset = bubble.getPhysicalOffset(paperSize);
+  const outlineWidth = bubble.getPhysicalOutlineWidth(paperSize);
+
+  const [w, h] = ceil2D(multiply2D(size, viewScale));
+  if (w < 1 || h < 1) { return; }
+
+  const ri = bubble.renderInfo!;
+  const ss = `${bubble.fontStyle} ${bubble.fontWeight} ${fontSize}px '${bubble.fontFamily}'`;
+
+  const c = {
+    viewScale: ceil2D(scale2D(viewScale, 1000)),
+    size: size,
+    offset: offset,
+    fontStyle: bubble.fontStyle,
+    fontWeight: bubble.fontWeight,
+    fontSize: fontSize,
+    lineSkip: bubble.lineSkip,
+    charSkip: bubble.charSkip,
+    fontFamily: bubble.fontFamily,
+    text: bubble.text,
+    direction: bubble.direction,
+    autoNewline: bubble.autoNewline,
+    fontColor: bubble.fontColor,
+    outlineWidh: outlineWidth,
+    outlineColor: bubble.outlineColor,
+    fontRenderVersion: bubble.fontRenderVersion,
+    fontCheck: document.fonts.check(ss),
+  };
+  const json = JSON.stringify(c);
+  
+  if (ri.textJson != json) {
+    // 変更が起きたときのみ
+    ri.textJson = json;
+
+    ri.textCanvas = document.createElement('canvas');
+    ri.textCtx = ri.textCanvas.getContext('2d')!;
+
+    const canvas = ri.textCanvas;
+    const ctx = ri.textCtx;
+
+    canvas.width = w;
+    canvas.height = h;
+
+    ctx.translate(w * 0.5, h * 0.5);
+
+    ctx.scale(...viewScale);
+    ctx.translate(...offset);
+
+    ctx.font = ss; // horizontal measureより先にないとだめ
+    //const text = `${bubble.text}:${bubble.pageNumber}`;
+    let text = bubble.text;
+    if (!document.fonts.check(`${bubble.fontStyle} ${bubble.fontWeight} 20px '${bubble.fontFamily}'`)) {
+      // どうもcheckが信用ならないので以下のコードは潰す
+      // text = "ロード中……";
+    }
+
+    const baselineSkip = fontSize * 1.5 * (1.0 + bubble.lineSkip);
+    const charSkip = fontSize * (1.0 + bubble.charSkip);
+    const rubySize = bubble.rubySize;
+    const rubyDistance = bubble.rubyDistance;
+    const m = measureText(bubble.direction as Direction, ctx, size[0] * 0.85, size[1] * 0.85, text, baselineSkip, charSkip, bubble.autoNewline);
+    const [tw, th] = [m.width, m.height];
+    const r = { x: - tw * 0.5, y: - th * 0.5, width: tw, height: th };
+
+    // 本体
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.fillStyle = bubble.fontColor;
+    drawText(bubble.direction as Direction, ctx, 'fill', r, text, baselineSkip, charSkip, rubySize, rubyDistance, m, bubble.autoNewline);
+
+    // フチ
+    if (0 < outlineWidth) {
+      ctx.globalCompositeOperation = 'destination-over';
+      ctx.strokeStyle = bubble.outlineColor;
+      ctx.lineWidth = outlineWidth;
+      ctx.font = ss;
+      ctx.lineJoin = 'round';
+      drawText(bubble.direction as Direction, ctx, 'stroke', r, text, baselineSkip, charSkip, rubySize, rubyDistance, m, bubble.autoNewline);
+    }
+  }
+
+  // 描き戻し
+  const canvas = ri.textCanvas;
+  try {
+    const rotation = Math.round(bubble.rotation * 10) / 10;
+
+    targetCtx.save();
+    if (rotation === 0 || rotation === 90 || rotation === 180 || rotation === 270) {
+      targetCtx.imageSmoothingEnabled = false;
+    }
+    targetCtx.scale(...reciprocal2D(viewScale));
+    targetCtx.drawImage(canvas, 0 - w * 0.5, 0 - h * 0.5);
+    targetCtx.restore();
+  }
+  catch (e) {
+    console.log(w, h, canvas.width, canvas.height, size);
+    throw e;
+  }
+}
